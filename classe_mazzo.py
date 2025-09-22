aed0f0ea7d859b069d9f8f7edfeafeb3e1e57d2c
JennyPersichini <nenny1096@gmail.com>
2025-09-22 16:22:33 +0200

    fix

---
+++ classe_mazzo.py
@@ -0,0 +1,39 @@
+from classe_carta import Carta
+
+
+class Mazzo():
+    def __init__(self):
+        semi = ["Bastoni", "Coppe", "Denari", "Spade"]
+        valori = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+        self.carte = [Carta(valore, seme) for seme in semi for valore in valori]
+
+    def conta(self):
+        return len(self.carte)
+
+    def __repr__(self):
+        return f"Mazzo di {self.conta()} carte"
+    
+    def _distribuisci(self,num):
+        count = self.conta()
+        n_carte_da_distribuire = min([count,num])
+        if count == 0:
+            raise ValueError("Non ci sono più carte nel mazzo")
+        self.carte = self.carte[:-n_carte_da_distribuire]
+        carte = self.carte[-n_carte_da_distribuire:]
+        return carte
+
+
+
+
+
+
+
+
+
+
+
+d = Mazzo()
+print(d.carte)
+print(d.conta())
+print(d)
--- mazzo_carte.py
+++ mazzo_carte.py
@@ -1,30 +1,5 @@
 from random import shuffle
 
-class Carta():
-    def __init__(self,)
-
-class Mazzo():
-    def __init__(self):
-        semi = ["Bastoni", "Coppe", "Denari", "Spade"]
-        valori = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-
-        self.carte = [Carta(valore, seme) for seme in semi for valore in valori]
-
-    def conta(self):
-        return len(self.carte)
-
-    def __repr__(self):
-        return f"Mazzo di {self.conta} carte"
-
-    def _distribuisci(self,num):
-        count = self.conta()
-        n_carte_da_distribuire = min([count,num])
-        if count == 0:
-            raise ValueError("Non ci sono più carte nel mazzo")
-        self.carte = self.carte[:-n_carte_da_distribuire]
-        carte = self.carte[-n_carte_da_distribuire:]
-        return carte
-    
     def mischia(self):
         if self.conta() < 40:
             raise ValueError("Posso mischiare solo il mazzo con tutte le carte")
